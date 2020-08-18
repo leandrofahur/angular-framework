622bcc1e4420f2219b1dc0c843818ffcdc3b2426
leandrofahur <leandrofahur@gmail.com>
2020-08-18 00:33:37 -0700

    property binding

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -3,13 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css'],
-  styles: [
-    `
-      h1 {
-        color: #ff0000;
-      }
-    `,
-  ],
+  styleUrls: ['./app.component.css'],
+  // styles: [
+  //   `
+  //     h1 {
+  //       color: #ff0000;
+  //     }
+  //   `,
+  // ],
 })
 export class AppComponent {}

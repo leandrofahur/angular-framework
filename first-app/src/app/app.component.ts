e6c97f696b9270dfbf2c7cb6bb1edef14f7466a2
leandrofahur <leandrofahur@gmail.com>
2020-08-17 21:24:38 -0700

    a bit of modules and components

---
+++ first-app/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  // styleUrls: ['./app.component.css'],
+  styles: [
+    `
+      h1 {
+        color: #ff0000;
+      }
+    `,
+  ],
+})
+export class AppComponent {}

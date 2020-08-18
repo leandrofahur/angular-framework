e292ce208bd1d2f2d10e1759616f242212831d37
leandrofahur <leandrofahur@gmail.com>
2020-08-18 14:26:02 -0700

    *nfIf and ngStyle

--- first-app/src/app/server/server.component.ts
+++ first-app/src/app/server/server.component.ts
@@ -9,7 +9,19 @@ export class ServerComponent {
   serverId: number = 10;
   serverStatus: string = 'offline';
 
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
   getServerStatus() {
     return this.serverStatus;
+  }
+
+  getColor() {
+    if (this.serverStatus === 'online') {
+      return '#00ff00';
+    } else {
+      return '#ff0000';
+    }
   }
 }

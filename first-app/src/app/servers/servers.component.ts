25efb474ed4b6cf66485a9c26a7141a76569db5f
leandrofahur <leandrofahur@gmail.com>
2020-08-18 14:07:02 -0700

    directives

--- first-app/src/app/servers/servers.component.ts
+++ first-app/src/app/servers/servers.component.ts
@@ -14,6 +14,7 @@ export class ServersComponent implements OnInit {
   allowNewServer: boolean = false;
   serverCreationStatus: string = `No server was created!!!`;
   serverName: string = ``;
+  serverCreated: boolean = false;
 
   constructor() {
     setTimeout(() => {
@@ -22,6 +23,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
+    this.serverCreated = true;
     this.serverCreationStatus = `Server was created! Name is ${this.serverName}`;
   }
 

cb42708db71c65756b8742a430b2a0a9df7bc4e9
leandrofahur <leandrofahur@gmail.com>
2020-08-18 23:35:12 -0700

    simple UI

--- sandbox/the-match-app/src/app/profile-list/profile-list.component.ts
+++ sandbox/the-match-app/src/app/profile-list/profile-list.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Profile } from '../shared/profile.model';
 
 @Component({
   selector: 'app-profile-list',
   templateUrl: './profile-list.component.html',
-  styleUrls: ['./profile-list.component.css']
+  styleUrls: ['./profile-list.component.css'],
 })
 export class ProfileListComponent implements OnInit {
+  profiles: Profile[] = [
+    new Profile(
+      'Jacob',
+      'male',
+      'New Zeland',
+      'https://randomuser.me/api/portraits/men/36.jpg'
+    ),
+    // new Profile(
+    //   'Byron',
+    //   'male',
+    //   'Australia',
+    //   'https://randomuser.me/api/portraits/men/85.jpg'
+    // ),
+    // new Profile(
+    //   'Isla',
+    //   'female',
+    //   'Australia',
+    //   'https://randomuser.me/api/portraits/women/38.jpg'
+    // ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

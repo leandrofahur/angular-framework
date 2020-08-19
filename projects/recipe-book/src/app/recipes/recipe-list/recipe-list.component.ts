cf3e3aaa5b6d069be05778a34bfee7ec15026c0a
leandrofahur <leandrofahur@gmail.com>
2020-08-18 17:03:04 -0700

    shapping recipe list

--- projects/recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ projects/recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,7 +11,7 @@ export class RecipeListComponent implements OnInit {
     new Recipe(
       'Cake',
       'This is a cake',
-      'https://unsplash.com/photos/kPxsqUGneXQ'
+      'https://images.unsplash.com/photo-1578985545062-69928b1d9587?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80'
     ),
   ];
   constructor() {}

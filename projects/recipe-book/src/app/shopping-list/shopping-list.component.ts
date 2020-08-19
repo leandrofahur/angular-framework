fbcd959d2c0d93a5db670913ba7888bf71ad1f4c
leandrofahur <leandrofahur@gmail.com>
2020-08-18 19:19:20 -0700

    shopping ingredient list

--- projects/recipe-book/src/app/shopping-list/shopping-list.component.ts
+++ projects/recipe-book/src/app/shopping-list/shopping-list.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients = [new Ingredient('egg', 3), new Ingredient('white flour', 100)];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

48c6a638110c6a617ccb2cdbb23f0e1b587c7bb9
leandrofahur <leandrofahur@gmail.com>
2020-08-20 00:04:27 -0700

    input the value inside the child component

--- assignments/cmp-databinding-assignment/src/app/app.component.ts
+++ assignments/cmp-databinding-assignment/src/app/app.component.ts
@@ -6,7 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
+
   onTimeInterval(time: number) {
-    console.log(time);
+    if (time % 2 === 0) {
+      this.evenNumbers.push(time);
+    } else {
+      this.oddNumbers.push(time);
+    }
   }
 }

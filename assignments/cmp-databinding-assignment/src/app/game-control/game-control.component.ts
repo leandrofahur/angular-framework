5823faa021b810d3d6327d619a4540c370bc1c93
leandrofahur <leandrofahur@gmail.com>
2020-08-19 23:22:08 -0700

    onStart() and onStop() behaviour implemented

--- assignments/cmp-databinding-assignment/src/app/game-control/game-control.component.ts
+++ assignments/cmp-databinding-assignment/src/app/game-control/game-control.component.ts
@@ -3,13 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-game-control',
   templateUrl: './game-control.component.html',
-  styleUrls: ['./game-control.component.css']
+  styleUrls: ['./game-control.component.css'],
 })
 export class GameControlComponent implements OnInit {
+  index: number = 0;
+  ref;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onStart() {
+    this.ref = setInterval(() => {
+      this.index++;
+      console.log(this.index);
+    }, 1000);
   }
 
+  onStop() {
+    clearInterval(this.ref);
+    this.index = 0;
+  }
 }

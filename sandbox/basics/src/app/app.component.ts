3b480cf650b955f0bf8cfdbd2c3d5792453c6d1b
leandrofahur <leandrofahur@gmail.com>
2020-08-20 03:14:53 -0700

    receiving data from the profile component

--- sandbox/basics/src/app/app.component.ts
+++ sandbox/basics/src/app/app.component.ts
@@ -5,4 +5,11 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  profiles: { name: string; email: string }[] = [];
+
+  onProfileAdded(data: { name: string; email: string }) {
+    this.profiles.push(data);
+    console.log(data);
+  }
+}

e3d507736d9e0acf3f574df639ef55a0946d86fc
leandrofahur <leandrofahur@gmail.com>
2020-08-19 18:36:00 -0700

    an emitter

--- cmp-databinding/src/app/app.component.ts
+++ cmp-databinding/src/app/app.component.ts
@@ -13,4 +13,23 @@ export class AppComponent {
       type: "server",
     },
   ];
+
+  onServerAdded(serverData: { serverName: string; serverContent: string }) {
+    this.serverElements.push({
+      type: "server",
+      name: serverData.serverName,
+      content: serverData.serverContent,
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {
+    serverName: string;
+    serverContent: string;
+  }) {
+    this.serverElements.push({
+      type: "blueprint",
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent,
+    });
+  }
 }
